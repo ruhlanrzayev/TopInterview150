b2123dc3a1951ef0de27c3223854cba74e38cce1
R33 <contactruhlanrzayev@gmail.com>
2025-02-20 19:38:54 +0400

    Majority Element JS

--- majorityElement.js
+++ majorityElement.js
@@ -1,1 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+*/
+
+var majorityElement = function(nums) {
+    nums.sort((a,b) => a-b);
+    if(nums.length==1) return nums[0]
+    else return nums[Math.floor(nums.length/2)]
+};
