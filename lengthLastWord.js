9e76aa3612d6d9a5d48cdeecdd79a578661de89a
R33 <contactruhlanrzayev@gmail.com>
2025-02-20 19:41:43 +0400

    Length Lasth Word JS

---
+++ lengthLastWord.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+*/
+
+var lengthOfLastWord = function(s) {
+    let newS = s.trim().split(" ");
+    return newS[newS.length-1].length
+};
